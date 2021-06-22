5eaa6635d2c03666dc36931e5d4d58d789b28075
reyhaneh.sp <reyhaneh.sanat@yahoo.co.uk>
2021-06-22 11:14:20 +0430

    commit done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
+import listMenu from "./config/sidebar";
 import Menu from "./view/layaout/SidebarMenu/index"
 
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ListMenu: [listMenu]
+        }
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <Menu/>
-    </div>
-  );
+    render() {
+
+        return (
+            <div className="App">
+                <Menu listMenu={this.state.ListMenu}/>
+            </div>
+        )
+    }
 }
 
 export default App;
--- src/view/layaout/SidebarMenu/index.js
+++ src/view/layaout/SidebarMenu/index.js
@@ -1,28 +1,38 @@
-import React from "react";
-import { Link, NavLink } from 'react-router-dom'
-import MetisMenu from '@metismenu/react';
+import React from 'react';
 
+class Menu extends React.Component {
 
-
-const Menu = () => (
-        <React.Fragment>
-            <MetisMenu>
-                <li><NavLink to="/" exact>Home</NavLink></li>
-                <li>
-                    <Link to="#" className="has-arrow">Other Pages</Link>
-                    <ul>
-                        <li><NavLink to="/about">About</NavLink></li>
-                        <li><NavLink to="/contact">Contact</NavLink></li>
-                    </ul>
-                </li>
-                <li>
-                    <Link to="#" className="has-arrow">External Pages</Link>
-                    <ul>
-                        <li><a href="https://github.com/onokumus/metismenujs" target="_blank" rel="noopener noreferrer">metismenujs</a></li>
-                        <li><a href="https://github.com/onokumus/metismenu" target="_blank" rel="noopener noreferrer">metismenu jquery</a></li>
-                    </ul>
-                </li>
-            </MetisMenu>
-        </React.Fragment>
-);
+    render() {
+        console.log(this.props.listMenu)
+        return (
+            <div className="row nav-org">
+                <div className="container-menu-desktop">
+                    <div className="wrap-header-mobile">
+                        <div className="btn-show-menu-mobile">
+					<span className="hamburger-box">
+					</span>
+                        </div>
+                    </div>
+                    <div className="wrap-main-nav">
+                        <div className="main-nav">
+                            <nav className="menu-desktop">
+                                <ul className="main-menu scroll-menu">
+                                    <li className="level-1">
+                                        <a href="/">داشبورد</a>
+                                    </li>
+                                    {this.props.listMenu.map((listMenu,index) => (
+                                        <li  key={index}>
+                                            <a href="">{listMenu.sections[index].title.name} </a>
+                                        </li>
+                                    ))
+                                    }
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
 export default Menu
